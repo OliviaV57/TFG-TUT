7112a1b29259ac7e866868cb6a286946cb7849ff
OliviaV57 <oliviavidalvelazquez@gmail.com>
2020-11-05 13:24:11 +0100

    He puesto el usuario y la contraseña

---
+++ MasaEstelarGalacticus.py
@@ -0,0 +1,15 @@
+import numpy as np
+import MDSqlTools as sql
+from matplotlib import pyplot as plt
+print(1)
+con = sql.connect("olivia.vidal@estudiante.uam.es", "TFG-TUT2020")
+print(2)
+the_query = """SELECT snapnum, MstarSpheroid FROM MDPL2.Galacticus
+            WHERE redshift = 0 
+            LIMIT 10"""
+print(3)
+data = con.execute_query(the_query)
+print(4)
+print (type (data))
+
+print ("Compila")
