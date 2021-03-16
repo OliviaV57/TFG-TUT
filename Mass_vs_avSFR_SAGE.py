f14c7479911262b9675d5559824d2759218728ee
OliviaV57 <oliviavidalvelazquez@gmail.com>
2021-03-16 18:57:54 +0100

    He intentado los arreglos que propone Violeta, hay algo que me corta el código en algún momento, es algún break o algún continue. He empezado con el proceso de debug pero no lo encuentro parece que se vuelve loco, le pido dos veces lo mismo en el mismo punto la primera vez lo hace y la segunda no, lo volveré a intentar.

--- Mass_vs_avSFR_SAGE.py
+++ Mass_vs_avSFR_SAGE.py
@@ -11,6 +11,7 @@ cm = plt.get_cmap('tab10')  # Colour map to draw colours from
 
 '''True o False si estoy haciendo un test o el programa entero'''
 test = True
+n = 1000
 
 '''Hago los bines de masa'''
 
@@ -52,6 +53,7 @@ for iiz in range(len(zsims)):
     '''Empezamos a leer linea a linea'''
     ff = open(ffsim, 'r')
     iline = -1  # Para que en el bucle empiece en el 0
+    #print(ff)
 
     for line in ff:
         iline += 1  # Cuenta las lineas que ya hay en el fichero. Quizá no haría falta ya que el fichero ya las tiene
@@ -60,14 +62,16 @@ for iiz in range(len(zsims)):
 
         if not char1.isdigit(): continue  # (para saltar header)
 
-        if test and iline > 10000:
-            break
+        if test and iline > n:
+            continue
+
 
         data = np.array(line.split(','),
                         dtype=float)  # Así ya va a ser float, data[0] = columna que cuenta, data[1] = masas, data[2] = SFR
 
         SFR = data[2]
         Mass = data[1]
+
 
         # loop por los bines las masas hasta el penultimo edge
         for ie, edge in enumerate(gedges[:-1]):
@@ -83,23 +87,64 @@ for iiz in range(len(zsims)):
 
         # Aquí llegan todas las SFR
 
+    # OJO
+    # Una vez leído todo el fichero calculamos las MEDIAS con los números totales en cada bin. Hay que leer todo
+    # el fichero y por eso salir del loop del fichero.
+    for ie, edge in enumerate(gedges[:-1]):
+        if ngal[ie] > 1:
+            medias[ie] = sumasfr[ie] / ngal[ie]
+    #print(medias, len(medias))
+
+    # OJO
+    # Segunda lectura del fichero para calcular los errores
+    for line in ff:
+
+        iline += 1
+        char1 = line[0]  # Primer caracter de la linea 1.
+
+        if not char1.isdigit():
+            continue  # (para saltar header)
+
+
+        if test and iline > n:
+            continue    #He puesto un continue, porque con el break se paraba el código aquí, sin embargo arriba
+                        # funciona con el break, no entiendo por qué. Sigo, dejando uno de cada a ver qué pasa.
+
+
+
+        data = np.array(line.split(','),
+                        dtype=float)  # Así ya va a ser float, data[0] = columna que cuenta, data[1] = masas, data[2] = SFR
+        SFR = data[2]
+
+'''
+
+
+
         for ie, edge in enumerate(gedges[:-1]):
-            if ngal[ie] > 1:
-                medias[ie] = sumasfr[ie] / ngal[ie]
-                sig[ie] = (((SFR - medias[ie]) ** 2) / (ngal[ie] - 1)) ** .5
-                errormedias[ie] = sig[ie] / ((ngal[ie]) ** .5)
+            # Addition for the numerator
+            sig[ie] = sig[ie] + (SFR - medias[ie]) ** 2
+            print(sig[ie])
 
-    print(errormedias)
+        print(SFR)
 
-    plt.plot(ghist, medias, marker='.', color=col, linewidth=0, label='SAGE z = ' + zsims[iiz] + '')
-    plt.errorbar(ghist, medias, yerr=errormedias, xerr=None, fmt='.', ecolor=None)
-    plt.ylabel('$Log_{10} \; $(SFR $[M_{\odot} \; h^{-1}\; yr^{-1}$])')
-    plt.xlabel('$Log_{10} \; (Masa \; [M_{\odot} \; h^{-1} $])')
-    plt.title('Media de la función SFR SAGE frente bines de masa de las galaxias')
-    plt.xlim(8.5, 12.5)
-    plt.legend()
+    # Una vez leído todo el fichero calculamos los ERRORES con los números totales en cada bin. Hay que leer todo
+    # el fichero y por eso salir del loop del fichero.
+    for ie, edge in enumerate(gedges[:-1]):
+        if ngal[ie] > 1:
+            sig[ie] = (sig[ie] / ((ngal[ie]-1)*ngal[ie]))**.5
+
+    print(medias,sig)
+
+    #plt.plot(ghist, medias, marker='.', color=col, linewidth=0, label='SAGE z = ' + zsims[iiz] + '')
+    #plt.errorbar(ghist, medias, yerr=errormedias, xerr=None, fmt='.', ecolor=None)
+    #plt.ylabel('$Log_{10} \; $(SFR $[M_{\odot} \; h^{-1}\; yr^{-1}$])')
+    #plt.xlabel('$Log_{10} \; (Masa \; [M_{\odot} \; h^{-1} $])')
+    #plt.title('Media de la función SFR SAGE frente bines de masa de las galaxias')
+    #plt.xlim(8.5, 12.5)
+    #plt.legend()
     # plotnom = 'C:/Users/Olivia/TFG-TUT/Figuras/avSFR_vs_Mass_Sage_z_' + zsims[iiz] + '.png'
     # print(plotnom)
     # plt.savefig(plotnom)
 
-    plt.show()
+    #plt.show()
+'''
